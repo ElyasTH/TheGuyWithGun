edbc07280d4e744ed581329f42ceb4141a1e395e
Elyas Tashakkori <87807482+ElyasTH@users.noreply.github.com>
2024-03-13 09:38:05 +0330

    -enemyBase & enemyMelee

---
+++ Unity Project/TheGuyWithAGun/Assets/_Project/Gameplay/Shared/Scripts/Managers/Enemy/EnemyMelee.cs
@@ -0,0 +1,20 @@
+﻿namespace _Project.Gameplay.Shared.Scripts.Managers.Enemy
+{
+    public class EnemyMelee: Enemy
+    {
+        protected override void TrackTarget()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UntrackTarget()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Attack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
